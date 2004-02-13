a29a724fccd02838e31630944158f0e5b888b937
Joseph Myers <jsm@polyomino.org.uk>
2004-02-13 23:18:04 +0000

    2004-02-13  Joseph S. Myers  <jsm@polyomino.org.uk>
    	* morse/morse.c: Update from NetBSD CVS.

--- morse/morse.c
+++ morse/morse.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: morse.c,v 1.12 2004/01/27 20:30:30 jsm Exp $	*/
+/*	$NetBSD: morse.c,v 1.13 2004/02/13 23:16:11 jsm Exp $	*/
 
 /*
  * Copyright (c) 1988, 1993
@@ -39,7 +39,7 @@ __COPYRIGHT("@(#) Copyright (c) 1988, 1993\n\
 #if 0
 static char sccsid[] = "@(#)morse.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: morse.c,v 1.12 2004/01/27 20:30:30 jsm Exp $");
+__RCSID("$NetBSD: morse.c,v 1.13 2004/02/13 23:16:11 jsm Exp $");
 #endif
 #endif /* not lint */
 
